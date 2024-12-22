6b606952cbb8dd175e5c832c028d3f87ec50e25c
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2024-12-22 19:32:01 +0100

    Amélioration de la mise en page du composant Footer, ajout de styles flexibles et mise à jour des dimensions des images pour une meilleure présentation.

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -3,53 +3,37 @@ import Image from 'next/image';
 
 export default function Footer() {
     return (
-        <div id="Contact" className="relative  bg-[#d5d1de] w-full h-[153px] border-solid border-black border-t">
+        <div id="Contact" className="flex flex-row items-center relative  bg-[#d5d1de] w-full h-[153px] border-solid border-black border-t">
             <div id="Infos">
-                <div>
+                <div className="flex flex-row items-center justify-items-start space-x-5 pl-12llllllll">
                     <Image
-                        className="absolute left-10 top-[25px]"
                         src="/mail.png"
                         alt="Email"
-                        width={32}
-                        height={32}
+                        width={64}
+                        height={64}
                     />
-                    <div className="text-[32px] absolute left-[86px] top-[25px] w-[521px] h-8">
+                    <div className="text-3xl font-medium">
                         kevyn.floresdacosta@outlook.com
                     </div>
                 </div>
-                <div className="">
-                    <Image
-                        className="absolute left-10 top-24"
-                        src="/phone.png"
-                        alt="Phone"
-                        width={32}
-                        height={32}
-                    />
-                    <div className="text-[32px] absolute left-[86px] top-24 w-[278px] h-8">
-                        +33 6 51 58 91 03
-                    </div>
-                </div>
+
             </div>
 
-            <div id="social">
+            <div id="social" className="flex flex-row items-center justify-items-end absolute right-12 space-x-16">
                 <a href="https://github.com/Kevynflrs" target="_blank" rel="noopener noreferrer">
                     <Image
-                        className="absolute left-[2030px] top-[45px]"
                         src="/github.png"
                         alt="GitHub"
                         width={64}
                         height={64}
-                        style={{ objectFit: 'cover' }}
                     />
                 </a>
                 <a href="https://www.linkedin.com/in/floreskevyn/" target="_blank" rel="noopener noreferrer">
                     <Image
-                        className="absolute left-[1910px] top-[45px]"
                         src="/linkedin.png"
                         alt="LinkedIn"
                         width={64}
                         height={64}
-                        style={{ objectFit: 'cover' }}
                     />
                 </a>
             </div>
